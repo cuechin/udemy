824f41f8d5c112db41c915cd3d8c1c8f7c64f272
Andrés Mora <amora@ingsw.com>
2024-01-23 16:29:03 -0500

    Initial commit

---
+++ iOS & Swift - The Complete iOS App Development Bootcamp/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by user213713 on 1/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
